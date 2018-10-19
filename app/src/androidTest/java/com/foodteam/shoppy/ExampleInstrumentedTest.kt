1db4c9c86fa4eeb16e5cabb2784353e7c572438e
jlmason13 <jlmason@mtu.edu>
2018-10-19 12:23:58 -0400

    Test commit comment

--- app/src/androidTest/java/com/foodteam/shoppy/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/foodteam/shoppy/ExampleInstrumentedTest.kt
@@ -15,6 +15,7 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    //Jayme's test commit comment
     @Test
     fun useAppContext() {
         // Context of the app under test.
