1e803eba6277ca4e74f687343fd458d59a674c70
Itari10 <elwinkle@mtu.edu>
2018-10-25 21:19:08 -0400

    Changes to Main Menu layout and Product Details layout. Main Menu buttons go to their designated screens.
    Signed-off-by: Itari10 <elwinkle@mtu.edu>

--- app/src/main/java/com/foodteam/shoppy/MainMenu.java
+++ app/src/main/java/com/foodteam/shoppy/MainMenu.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -13,6 +14,31 @@ public class MainMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-    }
+        //Main Menu Button: "LISTS"
+        Button gotoLists = findViewById(R.id.buttLists);
+        gotoLists.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Upon clicking "LISTS",
+                //Intent startIntent = new Intent(getApplicationContext(), MainMenu.class);
+                //startActivity(startIntent);
+                startActivity(new Intent(MainMenu.this, Lists.class));
+            }
+        });
 
+        //Main Menu Button: "MASTER LIST"
+        Button gotoMasterList = findViewById(R.id.buttMasterList);
+        gotoLists.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainMenu.this, MasterList.class));
+            }
+        });
+
+        //Main Menu Button: "SETTINGS"
+        Button gotoSettings = findViewById(R.id.buttSettings);
+        gotoLists.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainMenu.this, Settings.class));
+            }
+        });
+    }
 }
