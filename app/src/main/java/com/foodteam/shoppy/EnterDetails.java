ed443a543faabe15fc605fc1a22d7373e2ffdd37
Sophia <sfjensen@mtu.edu>
2018-11-08 23:59:55 -0500

    Implementation of database for EnterDetails and Master list, formatting for EnterDetails

--- app/src/main/java/com/foodteam/shoppy/EnterDetails.java
+++ app/src/main/java/com/foodteam/shoppy/EnterDetails.java
@@ -1,26 +1,102 @@
 package com.foodteam.shoppy;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class EnterDetails extends AppCompatActivity {
 
     SQLiteDatabase theDatabase;
+    String product = "test";     //this assignment is temporary for testing purposes
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_details);
+
+        Button submitBtn = (Button) findViewById(R.id.saveData);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                retreiveData();
+            }
+        });
+
     }
 
-    private void openTheDatabase() {
+    protected SQLiteDatabase openTheDatabase() {
         try {
-            theDatabase = this.openOrCreateDatabase("Shoppy", MODE_PRIVATE, null );
+            theDatabase = this.openOrCreateDatabase("shoppyDB", MODE_PRIVATE, null );
+            return theDatabase;
         } catch (Exception e) {
-            Log.e("DATABASE ERROR","Zofi: Error opening Database");
+            System.out.println("DATABASE ERROR" + " Zofi: Error opening Database");
+            return null;
         }
-
     }
+
+    protected void retreiveData() {
+        //TODO how to get product name from last page
+        TextView priceTxt = (TextView) findViewById(R.id.enterPrice);
+        TextView sizeTxt  = (TextView) findViewById(R.id.enterSize);
+        TextView brandTxt = (TextView) findViewById(R.id.enterBrand);
+        TextView storeTxt = (TextView) findViewById(R.id.enterStore);
+
+        Float newPrice   = Float.parseFloat( (String) priceTxt.getText() );
+        Float newSize    = Float.parseFloat( (String) sizeTxt.getText() );
+        String newBrand    = (String) brandTxt.getText();
+        String newStore    = (String) storeTxt.getText();
+
+        System.out.println("ED here1");
+        updateProductDetails( product, newPrice, newSize, newBrand, newStore);
+    }
+
+    protected void updateProductDetails( String product, Float newPrice, Float newSize, String newBrand, String newStore) {
+        System.out.println("ED here2");
+        Cursor productQuery = theDatabase.rawQuery("select from MasterList where product = '" + product + "';", null);
+        System.out.println("ED here3");
+        if( productQuery.getCount() == 1 ){
+            //TODO need to update details of that product Table, ?check for brand and size match
+
+            Cursor checkBrandSize = theDatabase.rawQuery( "select from" + product +
+                " where brand = '" + newBrand + "', size = '" + newSize + "';", null);
+            checkBrandSize.moveToFirst();
+
+            if (checkBrandSize.getCount() == 1) {   //must update existing row
+                int lowestPriceColumn   = checkBrandSize.getColumnIndex("lowestPrice");
+                float lowest = Float.parseFloat( checkBrandSize.getString(lowestPriceColumn) );
+                if ( lowest > newPrice ) { // if newPrice < the current lowest price, update lowest price to newPrice, and store to newStore
+                    //TODO cont
+                }
+
+            } else {    //combo of brand and size does not exist so create new row in product
+                theDatabase.execSQL("insert into" + product + "(brand, size, frequency, avgPrice, lowestPrice, highestPrice, store, totalSpent)" +
+                        "values ( " + newBrand + ", " + newSize + ", 1, " + newPrice + ", " + newPrice + ", " + newPrice + ", " + newStore + ", " + newPrice + ");" );
+            }
+
+        } else {
+            //add product to MasterList
+            theDatabase.execSQL("insert into MasterList " +
+                    "(product, frequency, avgPrice, lowestPrice, totalSpent) " +
+                    "values ( " + product + ", 0, 0.00, 0.00, 0.00 );");            //depending on trigger to update, TODO create trigger to update
+
+            //create a table with that exact name of product
+            theDatabase.execSQL("CREATE TABLE IF NOT EXISTS " + product +
+                    "(brand VARCHAR, size integer, frequency integer, avgPrice float(9,2), lowestPrice float (9,2), highestPrice float(9,2), store VARCHAR, totalSpent float(9,2) ) " +
+                    "primary key(brand, size) ;");
+
+            //insert new data from into the new table data
+            theDatabase.execSQL( "insert into " + product +
+                    "(brand, size, frequency, avgPrice, lowestPrice, highestPrice, store, totalSpent ) " +
+                    "values ( " + newBrand + ", " + newSize + ", 1, " + newPrice + ", " + newPrice + ", " + newPrice + ", " + newStore + ", " + newPrice + ");" );
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/foodteam/shoppy/MasterList.java
+++ app/src/main/java/com/foodteam/shoppy/MasterList.java
@@ -11,27 +11,40 @@ import android.util.Log;
 import android.view.View;
         import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 
 public class MasterList extends AppCompatActivity {
 
     SQLiteDatabase theDatabase;
+    TableLayout theTable;
+    int[] filters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        System.out.println("here0");
         openTheDatabase();
         setContentView(R.layout.activity_master_list);
+        theTable = (TableLayout) findViewById(R.id.theTable);
+
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if(extras == null) {
+                filters = null;
+            } else {
+                filters = extras.getIntArray("filtersList");
+            }
+        }
         addMasterListProductData();
-
-
 
         Button gotoFrontBtn = (Button) findViewById(R.id.gotoFilters );
         gotoFrontBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent startIntent = new Intent( getApplicationContext(), Filters.class );    // MasterListFilters.class complaining because don't have current master version
+                Intent startIntent = new Intent( getApplicationContext(), MasterListFilters.class );    // MasterListFilters.class complaining because don't have current master version
                 startActivity(startIntent);
 
             }
@@ -39,18 +52,18 @@ public class MasterList extends AppCompatActivity {
 
     }
 
-
-    private void openTheDatabase() {
+    protected SQLiteDatabase openTheDatabase() {
         try {
-            theDatabase = this.openOrCreateDatabase("Shoppy", MODE_PRIVATE, null );
+            theDatabase = this.openOrCreateDatabase("shoppyDB", MODE_PRIVATE, null );
+            return theDatabase;
         } catch (Exception e) {
-            Log.e("DATABASE ERROR","Zofi: Error opening Database");
+            System.out.println("DATABASE ERROR" + " Zofi: Error opening Database");
+            return null;
         }
-
     }
 
-    private void addMasterListProductData() {
-        Cursor itterate = theDatabase.rawQuery( "select * from MasterList", null);
+    protected int addMasterListProductData() {
+        Cursor itterate = theDatabase.rawQuery( "select * from MasterList;", null);
 
         int productColumn       = itterate.getColumnIndex("product");
         int freqColumn          = itterate.getColumnIndex("frequency");
@@ -58,45 +71,59 @@ public class MasterList extends AppCompatActivity {
         int lowestPriceColumn   = itterate.getColumnIndex("lowestPrice");
         int totalSpentColumn    = itterate.getColumnIndex("totalSpent");
 
-        LinearLayout attOneCol       = (LinearLayout) findViewById(R.id.AttOneCol);
-        LinearLayout attTwoCol      = (LinearLayout) findViewById(R.id.AttTwoCol);
-        LinearLayout attThreeCol    = (LinearLayout) findViewById(R.id.AttThreeCol);
-        LinearLayout attFourCol     = (LinearLayout) findViewById(R.id.AttFourCol);
-        LinearLayout attFiveCol     = (LinearLayout) findViewById(R.id.AttFiveCol);
-        LinearLayout attButtonCol   = (LinearLayout) findViewById(R.id.AttButtonCol);
+        Button   detailsButton   = new Button(this);
+        detailsButton.setText("Details");
 
         itterate.moveToFirst();
 
+        System.out.println("here1");    //track where error occurs
+        int i = 2;
+
         if (itterate != null && (itterate.getCount() > 0)) {
             do{
-               String curProduct = itterate.getString(productColumn);
-               String curFreq = itterate.getString(freqColumn);
-               String curAvg = itterate.getString(avgPriceColumn);
-               String curLow = itterate.getString(lowestPriceColumn);
-               String curTotalSpent = itterate.getString(totalSpentColumn);
+                System.out.println("here" + i); i++;
+                TextView curProduct      = createTextView( itterate.getString(productColumn)     );
+                TextView curFreq         = createTextView( itterate.getString(freqColumn)        );
+                TextView curAvg          = createTextView( itterate.getString(avgPriceColumn)    );
+                TextView curLow          = createTextView( itterate.getString(lowestPriceColumn) );
+                TextView curTotalSpent   = createTextView( itterate.getString(totalSpentColumn)  );
 
-               addTextViewToLinearLayout( curProduct, attOneCol);              //TODO Must figure out how to implement order based on filter
-               addTextViewToLinearLayout( curFreq, attTwoCol);
-               addTextViewToLinearLayout( curAvg, attThreeCol);
-               addTextViewToLinearLayout( curLow, attFourCol);
-               addTextViewToLinearLayout( curTotalSpent, attFiveCol);
-               addButtonToLinearLayout( "Details", attButtonCol);
-
+                createNewRow( curProduct, curFreq, curAvg, curLow, curTotalSpent, detailsButton);   //details Button has no onclickListener
             } while ( itterate.moveToNext() );
 
         }
+        return 1;
     }
 
+    private void createNewRow ( TextView curProduct, TextView curFreq, TextView curAvg, TextView curLow, TextView curTotalSpent, Button detailsButton ) {
+        //TODO Must figure out how to implement order based on filter
+        TableRow row = new TableRow(this);
 
-    private void addTextViewToLinearLayout (String toDisplay, LinearLayout layout){
+        row.addView(curProduct);
+        row.addView(curFreq);
+        row.addView(curAvg);
+        row.addView(curLow);
+        row.addView(curTotalSpent);
+        row.addView(detailsButton);
+
+        theTable.addView(row);
+    }
+
+    private TextView createTextView (String toDisplay ){
         TextView text = new TextView( this );
         text.setText( toDisplay );
-        layout.addView( text );
+        return text;
     }
 
-    private void addButtonToLinearLayout (String toDisplay, LinearLayout layout){
-        Button text = new Button( this );
-        text.setText( toDisplay );
-        layout.addView( text );
-    }
 }
+
+/**
+ input from Filters format for array:
+ 0 or 1  name  (0 means don't display, 1 means display)
+ 0 or 1	 freq
+ 0 or 1	 avg
+ 0 or 1	 lowest
+ 0 or 1  totalprice
+ 0 - 5   radio buttons
+            0 = alpha, 1 = freq low to high, 2 = freq high to low, 3 = lowest price, 4 = total spent low to high, 5 = total spent high to low
+ **/
